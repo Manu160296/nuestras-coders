7f172f53eea8ab3e6ec7f60c13d7fb5845f5248b
Manu160296 <fmanuela499@gmail.com>
2017-11-13 21:35:27 -0500

    version 2.0 finalizada

--- js/app.js
+++ js/app.js
@@ -1,43 +1,27 @@
-
-var container = document.getElementsByClassName('hide-students')[0];
+var container = document.getElementsByClassName('hide-students');
 var select = document.getElementById('select');
-var options =document.getElementsByClassName('option')
- select.onchange = function () {
-   if(container.classList.contains('first')){
-     console.log('hola')
-     container.classList.remove('hide-students');
-     container.classList.add('show-students');
-   } else if (container.classList.contains('second')) {
-     container.classList.remove('hide-students');
-     container.classList.add('show-students');
-   }
+var options = document.getElementsByClassName('option');
+select.addEventListener("change", showStudents);
+
+function showStudents(e) {
+  if (select.value == "hidden") {
+    container[0].style.display = "none";
+    container[1].style.display = "none";
+    container[2].style.display = "none";
+
+  } else if (select.value == "fourth-class") {
+    container[0].style.display = "block";
+    container[1].style.display = "none";
+    container[2].style.display = "none";
+  } else if (select.value == "fifth-class") {
+    container[1].style.display = "block";
+    container[0].style.display = "none";
+    container[2].style.display = "none";
+  } else if (select.value == "sixth-class") {
+    container[2].style.display = "block";
+    container[0].style.display = "none";
+    container[1].style.display = "none";
+  }
+
 
 }
-console.log(select)
-
-
-
-/*
-console.log(options);
-for (var i = 0; i < options.length; i++) {
-  options[i].addEventListener('onchange', showStudents)
-  console.log(options[i].getAttribute('value'))
-}
-function showStudents() {
-  console.log("hola")
-}
-
-
------
-
-console.log('evento')
-if (container[0].classList.value=='hide-students') {
-   container[0].classList.remove('hide-students');
-   container[0].classList.add('show-students');
-} else if (container[1].classList.value=='hide-students') {
-  container[0].classList.remove('show-students');
-  container[0].classList.add('hide-students');
-  container[1].classList.remove('hide-students');
-  container[1].classList.add('show-students');
-}
-*/
